946219001fb843b783a4abf13826d432f26f7eb5
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-10-02 08:23:43 +0200

    fix: make cookie consent window consistent in dark mode

--- js/cookieconsentinit.js
+++ js/cookieconsentinit.js
@@ -1,6 +1,7 @@
+var isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 window.cookieconsent.initialise({
     palette: {
-        popup: { background: "#ffffff" },
+        popup: { background: isDarkMode ? "#000000" : "#ffffff" },
         button: { background: "#0000ff" },
     },
     theme: "classic",
